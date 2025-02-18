5396cd391ededb313a4081efa2a2e7a14fa4146b
biel2112 <gabrielc_morais@hotmail.com>
2025-02-18 00:25:43 -0300

    Finalizando a aula 4

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import aulas.aula3.Carro;
-import aulas.aula3.SmartTV;
-
-import java.util.Arrays;
+import aulas.aula4.Conta;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,15 +9,22 @@ public class Main {
 //        System.out.println(carro.ligarOuDesligar());
 
         //Testando a classe SmartTV
-        SmartTV tv = new SmartTV("SONY", 45, 90, 29, false);
-        System.out.println(tv);
-        tv.ligarDesligar();
-        System.out.println(tv);
-        tv.aumentarVolume();
-        tv.aumentarVolume();
-        System.out.println(tv);
-        tv.mudarCanal(26);
-        System.out.println(tv);
+//        SmartTV tv = new SmartTV("SONY", 45, 90, 29, false);
+//        System.out.println(tv);
+//        tv.ligarDesligar();
+//        System.out.println(tv);
+//        tv.aumentarVolume();
+//        tv.aumentarVolume();
+//        System.out.println(tv);
+//        tv.mudarCanal(26);
+//        System.out.println(tv);
+
+//        Testando classe Conta
+        Conta conta = new Conta("Gabriel", 190.0);
+        System.out.println(conta);
+        conta.depositar(19);
+        System.out.println(conta);
+        conta.sacar(220);
 
 
 
